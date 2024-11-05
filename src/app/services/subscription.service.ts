881e06f45ba0152e3efac393c1e025dc15278dde
Maram-web <maram.manai@esprit.tn>
2024-11-05 09:05:56 +0000

    deploy srv

--- src/app/services/subscription.service.ts
+++ src/app/services/subscription.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs/internal/Observable';
 })
 export class SubcriptionService {
 
-  apiUrl: string ="http://135.234.225.34:9001/api/subscription";
+  apiUrl: string ="http://172.212.121.182:9001/api/subscription";
 
   constructor(private http: HttpClient) { }
 
